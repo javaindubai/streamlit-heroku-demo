f23b2af91a7e6716b171534836b02a01ff467ff6
Javalingappa <javaindubai@gmail.com>
2022-12-14 04:05:27 +0400

    Added new logo

--- MainApp.py
+++ MainApp.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
+from PIL import Image
+
+img = Image.open('logo.png')
+
+st.set_page_config(page_icon=img)
+
 st.title("Welcome to Heroku Deployment")
 
 st.markdown("----")
 st.subheader("How do we deploy Streamlit applications on Heroku?")
+
+st.image(img)
